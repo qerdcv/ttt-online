c536e6b3a188a6ac91fce6804325dc6c9c699932
qerdcv <vadym.tishchenko2@gmail.com>
2021-12-30 19:10:08 +0200

    Add hot reload for server

--- server/main.py
+++ server/main.py
@@ -10,7 +10,7 @@ def create_app() -> web.Application:
 
 def main():
     app = create_app()
-    web.run_app(host='0.0.0.0', port=4444, app=app)
+    web.run_app(host='0.0.0.0', port=8000, app=app)
 
 
 if __name__ == '__main__':
