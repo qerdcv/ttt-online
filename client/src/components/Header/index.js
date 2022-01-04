c8c2678a46ff03a9a492a3bf7b24a12eecad55e7
qerdcv <vadym.tishchenko2@gmail.com>
2022-01-04 03:54:29 +0200

    Add home routing

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,6 +5,8 @@ import styles from "components/Header/header.module.scss"
 export const Header = () => {
   return (
     <header className={styles.header}>
+      <Link to="/" className={styles.home}>Home</Link>
+
       <nav className={styles.navbar}>
         <Link to="/auth/login" className={styles.navbarItem}>Sign IN</Link>/
         <Link to="/auth/register" className={styles.navbarItem}>Sign UP</Link>
