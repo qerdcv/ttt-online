04c6d21198af2c587ce7b806ebf021ccc4d997ef
qerdcv <vadym.tishchenko2@gmail.com>
2022-02-10 22:38:23 +0200

    Fix pool issues

--- server/main.py
+++ server/main.py
@@ -1,23 +1,22 @@
 import os
-import asyncio
 
 import asyncpg
-import nest_asyncio
 from aiohttp import web
 
 from src.routes import routes
 from src.db import create_db
 
 
+async def init_pool(app):
+    app['pool'] = await asyncpg.create_pool(
+        os.environ['DB_URI']
+    )
+
+
 def create_app() -> web.Application:
     app = web.Application()
-    loop = asyncio.get_event_loop()
-    # TODO: Solve problem "RuntimeError: This event loop is already running" without nest_asyncio library
-    nest_asyncio.apply(loop)
-    app['pool'] = loop.run_until_complete(asyncpg.create_pool(
-        os.environ['DB_URI']
-    ))
     app.add_routes(routes)
+    app.on_startup.append(init_pool)
     app.on_startup.append(create_db)
     return app
 
