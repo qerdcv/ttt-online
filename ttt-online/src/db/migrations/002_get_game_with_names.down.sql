28a9f953ddb37b9f81f5b2420ac86e5c837a0735
Drozd0f <600dan600@gmail.com>
2022-08-02 10:23:38 +0300

    Rebasing create_game() function to complex query

--- ttt-online/src/db/migrations/002_get_game_with_names.down.sql
+++ ttt-online/src/db/migrations/002_get_game_with_names.down.sql
@@ -1,3 +1,1 @@
 DROP VIEW IF EXISTS get_games;
-
-DROP FUNCTION IF EXISTS create_game(game_owner_id INTEGER);
--- ttt-online/src/db/queries/create_game.sql
+++ ttt-online/src/db/queries/create_game.sql
@@ -1,1 +1,13 @@
-SELECT * FROM create_game(game_owner_id:=$1);
+-- SELECT * FROM create_game(game_owner_id:=$1);
+with g as ( insert into games (owner_id) values ($1) returning * )
+select
+    g.*,
+    ow.username as owner_name,
+    op.username as opponent_name,
+    win.username as winner_name,
+    cp.username as current_player_name
+from g
+left join users ow on ow.id = g.owner_id
+left join users op on op.id = g.opponent_id
+left join users win on win.id = g.winner_id
+left join users cp on cp.id = g.current_player_id;
